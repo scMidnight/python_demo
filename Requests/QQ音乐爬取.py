e09b9f9cc05dd2f7434cbc6705e215eee35893f8
midnight_corp <3741637@qq.com>
2019-07-10 19:55:00 +0800

    修改QQ音乐爬取部分代码

--- Requests/QQ音乐爬取.py
+++ Requests/QQ音乐爬取.py
@@ -3,16 +3,43 @@
 
 import requests
 # 引用requests库
-
-res = requests.get('https://c.y.qq.com/soso/fcgi-bin/client_search_cp?ct=24&qqmusic_ver=1298&new_json=1&remoteplace=txt.yqq.song&searchid=65937373069588489&t=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p=1&n=10&w=%E5%91%A8%E6%9D%B0%E4%BC%A6&g_tk=542682653&loginUin=374137689&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=0')
-# 调用get方法，下载这个字典
-json_music = res.json()
-for item in json_music['data']['song']['list']:
-    print(item['name'])
-    # 以name为键，查找歌曲名
-    print('所属专辑：' + item['album']['name'])
-    # 查找专辑名
-    print('播放时长：' + str(item['interval']) + '秒')
-    # 查找播放时长
-    print('播放链接：https://y.qq.com/n/yqq/song/' + item['mid'] + '.html\n\n')
-    # 查找播放链接
+music_name = input("请输入歌曲名称")
+for i in range(5):
+    params = {
+            'ct': '24',
+            'qqmusic_ver': '1298',
+            'new_json': '1',
+            'remoteplace': 'txt.yqq.song',
+            'searchid': '65537859516187558',
+            't': '0',
+            'aggr': '1',
+            'cr': '1',
+            'catZhida': '1',
+            'lossless': '0',
+            'flag_qc': '0',
+            'p': str(i+1),
+            'n': '10',
+            'w': music_name,
+            'g_tk': '5381',
+            'loginUin': '0',
+            'hostUin': '0',
+            'format': 'json',
+            'inCharset': 'utf8',
+            'outCharset': 'utf-8',
+            'notice': '0',
+            'platform': 'yqq.json',
+            'needNewCode': '0'
+        }
+    url = 'https://c.y.qq.com/soso/fcgi-bin/client_search_cp'
+    res = requests.get(url, params=params)
+    # 调用get方法，下载这个字典
+    json_music = res.json()
+    for item in json_music['data']['song']['list']:
+        print(item['name'])
+        # 以name为键，查找歌曲名
+        print('所属专辑：' + item['album']['name'])
+        # 查找专辑名
+        print('播放时长：' + str(item['interval']) + '秒')
+        # 查找播放时长
+        print('播放链接：https://y.qq.com/n/yqq/song/' + item['mid'] + '.html\n\n')
+        # 查找播放链接
